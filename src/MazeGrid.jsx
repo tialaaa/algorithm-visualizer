9cddf62096dbc95b964bc30f5ad1e6a6b72b9c23
Tiala Young <tialayoung@gmail.com>
2024-04-24 17:02:10 -0600

    Add generateMaze funtion to randomly generate new mazes

--- src/MazeGrid.jsx
+++ src/MazeGrid.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "./MazeGrid.css";
 
 export default function MazeGrid() {
-  let maze = [
+  let initialMaze = [
     ["wall", "wall", "wall", "wall"],
     ["start", "path", "path", "wall"],
     ["wall", "wall", "path", "wall"],
@@ -9,9 +10,38 @@ export default function MazeGrid() {
     ["wall", "wall", "wall", "wall"],
   ];
 
+  const [maze, setMaze] = useState(initialMaze);
+
+  function generateMaze(height, width) {
+    let matrix = [];
+
+    for (let i = 0; i < height; i++) {
+      let row = [];
+
+      for (let j = 0; j < width; j++) {
+        let cell = Math.random();
+        if (cell < 0.5) {
+          row.push("wall");
+        } else {
+          row.push("path");
+        }
+      }
+
+      matrix.push(row);
+    };
+
+    console.log(matrix)
+    matrix[1][0] = 'start';
+    matrix[height - 2][width - 1] = 'end';
+
+    setMaze(matrix);
+  };
+
   return (
     <div className="maze-container">
-      <button className="refresh-button">Refresh Maze</button>
+      <button className="refresh-button" onClick={() => generateMaze(5, 6)}>
+        Refresh Maze
+      </button>
       <div className="maze">
         {maze.map((row, rowIndex) => (
           <div className="row">

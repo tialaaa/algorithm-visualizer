22c381f20f1cc7c1decf29685942ee5371f8b6a3
Tiala Young <tialayoung@gmail.com>
2024-04-24 16:32:41 -0600

    Add refresh button

--- src/MazeGrid.jsx
+++ src/MazeGrid.jsx
@@ -6,11 +6,13 @@ export default function MazeGrid() {
     ["start", "path", "path", "wall"],
     ["wall", "wall", "path", "wall"],
     ["wall", "wall", "path", "end"],
+    ["wall", "wall", "wall", "wall"],
   ];
 
   return (
-    <>
-      <div>
+    <div className="maze-container">
+      <button className="refresh-button">Refresh Maze</button>
+      <div className="maze">
         {maze.map((row, rowIndex) => (
           <div className="row">
             {row.map((cell, cellIndex) => (
@@ -19,6 +21,6 @@ export default function MazeGrid() {
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
